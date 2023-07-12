79807884e2259a14a6550b19d7c1c716d5b2cc5a
marciobrito1981 <marcio.brito@outlook.com>
2023-07-12 09:48:37 -0300

    Primeiro Commit do projeto

--- spec/controller/addresses_spec.rb
+++ spec/controller/addresses_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe AddressesController, type: :controller do
-    it 'request index without auth and return 401' do
+    it 'request index without token and return 401' do
         get :index 
         expect(response).to have_http_status(:unauthorized)  
     end
